ded40fa9ef1dbbf8353ee8d90b4f7558e62f010e
dudka-54 <kovboimolodoi@yandex.ru>
2026-06-02 20:34:40 +0400

    add POJOs

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.item.model;
 
+import ru.practicum.shareit.request.ItemRequest;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class Item {
+    private Integer id;
+    private String name;
+    private String description;
+    private boolean available;
+    private Integer owner;
+    private ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.user;
 
+import jakarta.validation.constraints.Email;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class User {
+    private Integer id;
+
+    private String name;
+
+    @Email
+    private String email;
 }
